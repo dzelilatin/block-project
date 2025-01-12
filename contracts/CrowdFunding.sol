52c2adc1e75b2a0fb9112a369f54b2c45a7b54d8
Dželila <dzelila.tinjak@stu.ibu.edu.ba>
2025-01-12 14:01:55 +0100

    updating contract

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -12,4 +12,59 @@ contract CrowdFunding {
         address[] donators;
         uint256[] donations;
     }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline) public returns (uint256) {
+        
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline must be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;  // so it returns the actual length of campaign. 
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id]; // so it gives us the exact campaign we are looking for 
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+// returns address and amount donated of donator 
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+//gets all campaigns that a user created
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns); //keeps track of number of campaigns created
+
+        for(uint i = 0; i<numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item; 
+        }
+    
+    return allCampaigns;
+    }
 }
